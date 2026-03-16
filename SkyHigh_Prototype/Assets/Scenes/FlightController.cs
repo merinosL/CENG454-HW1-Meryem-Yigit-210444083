73605300a4ac5806d532066fcfef89f7f6d19987
Meryem Yiğit <meryemyiit@yahoo.com.tr>
2026-03-16 13:46:43 +0300

    feat: create FlightController skeleton and setup Rigidbody

---
+++ SkyHigh_Prototype/Assets/Scenes/FlightController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = 45f;
+    [SerializeField] private float yawSpeed = 45f;
+    [SerializeField] private float rollSpeed = 45f;
+    [SerializeField] private float thrustSpeed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+}
